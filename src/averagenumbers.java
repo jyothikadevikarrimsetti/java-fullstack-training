4372aeed4562283dab9ff6f71681ba749151265a
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-29 16:16:13 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/averagenumbers.java
@@ -0,0 +1,10 @@
+public class averagenumbers {
+    public static void main(String args[]){
+        int a =10;
+        int b =20;
+        int c =30;
+        int sum =a+b+c;
+        double average = sum/3.0;
+        System.out.print(average);
+    }
+}

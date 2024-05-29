c53f41bebd3deaa84bf4df0ad00c7a722ef5f8bc
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-29 12:11:10 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ConvertingDoubletoInt.java
@@ -0,0 +1,8 @@
+public class ConvertingDoubletoInt {
+    public static void main(String args[]){
+      double num =20;
+      int data = (int)num;
+      System.out.println(num);
+      System.out.println(data);
+    }
+}

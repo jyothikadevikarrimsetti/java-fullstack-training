8c34f8af5de7ed913df0e48bd8df10c3d0c39e9a
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-29 11:10:51 +0530

    Javafullstack

---
+++ src/ConvertIntIntoDouble.java
@@ -0,0 +1,12 @@
+public class ConvertIntIntoDouble {
+    public static void main(String args[]){
+        int intValue=40;
+        double doubleValue;
+        doubleValue=intValue;
+        System.out.println(intValue);
+        System.out.println(doubleValue);
+
+        System.out.println("double");
+
+    }
+}
---
+++ src/datatypes.java
@@ -0,0 +1,19 @@
+public class datatypes {
+    public static void main(String args[]) {
+        char ab = 'k';
+        int il = 498;
+        byte by = 4;
+        short sh = 123;
+        double dh=4.9888;
+        float fl = 4.988888f;
+        long ln = 12123;
+        System.out.println(ab);
+        System.out.println(il);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println( dh);
+        System.out.println(fl);
+        System.out.println(ln);
+
+    }
+}

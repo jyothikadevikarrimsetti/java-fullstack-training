8288df27845618ba56e76baae421e6ce1643506c
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-29 15:14:39 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/DataTypesConversions.java
@@ -0,0 +1,7 @@
+public class DataTypesConversions {
+    public static void main(String args[]){
+        int a=10;
+        float fl = (float)a;
+        System.out.println(a);
+    }
+}
---
+++ src/intToDouble.java
@@ -0,0 +1,7 @@
+public class intToDouble {
+    public static void main(String args[]){
+        int i=42;
+        double d=(double) i;
+        System.out.println(d);
+    }
+}

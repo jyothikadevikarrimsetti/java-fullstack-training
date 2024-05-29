9c2045e582122b8a09f053f5b313ebeaf029666c
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-29 12:37:50 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ConvertingFloatToDouble.java
@@ -0,0 +1,8 @@
+public class ConvertingFloatToDouble {
+    public static void main(String args[]){
+        float f=10;
+        double d=( float)f;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}

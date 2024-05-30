82d8485cc0367504a23cf4165ae366d5713c0ac3
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 14:10:54 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/StringmathMethods.java
@@ -0,0 +1,8 @@
+public class StringmathMethods {
+    public static void main(String args[]){
+        String a = "(10,20)";
+        System.out.println(Math.max(10,20));
+        System.out.println(Math.min(10,20));
+
+    }
+}

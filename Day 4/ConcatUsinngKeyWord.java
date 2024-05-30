3febd0832ec5e66c0b8c7a7130160f1fae6d8970
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 13:16:14 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/ConcatUsinngKeyWord.java
@@ -0,0 +1,7 @@
+public class ConcatUsinngKeyWord {
+    public static void main(String args[]){
+        String name1 = "Swarnandhra";
+        String name2 = "college";
+        System.out.println(name1 .concat(name2));
+    }
+}

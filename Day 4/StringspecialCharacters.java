aa922ab74e74e369dd8c75af2265503a70e6d746
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 14:23:12 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/StringspecialCharacters.java
@@ -0,0 +1,10 @@
+public class StringspecialCharacters {
+    public static void main(String args[]){
+        String quote = "There is a way to do it \"Better\" find it";
+        String day ="don\'t be afraid";
+        String strive = "the future\\is bright";
+        System.out.println(quote);
+        System.out.println(day);
+        System.out.println(strive);
+     }
+}

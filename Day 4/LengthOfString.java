7f6d961262012ee184406ebebd4f087b26dac95a
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 12:49:14 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/LengthOfString.java
@@ -0,0 +1,6 @@
+public class LengthOfString {
+    public static void main(String args[]){
+        String name = "Jyothika Devi";
+        System.out.print("The Length Of The String Is:"+name.length());
+    }
+}

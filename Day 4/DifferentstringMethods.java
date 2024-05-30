e6b013f1c4fa1d19de29b9d6a75731622356dac2
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 13:03:07 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/DifferentstringMethods.java
@@ -0,0 +1,9 @@
+public class DifferentstringMethods {
+    public static void main(String args[]){
+        String quote = "love it or leave it";
+        System.out.println("length of the string is:"+quote.length());
+        System.out.println("the upper case of the string is:"+quote.toUpperCase());
+        System.out.println("the lower case of the string is:"+quote.toLowerCase());
+        System.out.println(quote.indexOf("love" ));
+    }
+}

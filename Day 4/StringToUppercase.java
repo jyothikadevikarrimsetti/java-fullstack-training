447dfcd0d1e8f14e5713ab9c5a1b306d98e92799
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 12:54:51 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/StringToUppercase.java
@@ -0,0 +1,6 @@
+public class StringToUppercase {
+    public static void main(String args[]){
+        String name ="ramesh naidu";
+        System.out.print("String to upper case is:"+name.toUpperCase());
+    }
+}

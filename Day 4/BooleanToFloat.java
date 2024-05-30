9bfac988e202a253aaba0a8f5fb8c9e09ddca8ed
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 11:16:13 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/BooleanToFloat.java
@@ -0,0 +1,7 @@
+public class BooleanToFloat {
+    public static void main(String args[]){
+        boolean boolVal = false;
+        float floatVal = boolVal ? 1.0f : 0.0f;
+        System.out.println(boolVal);
+    }
+}

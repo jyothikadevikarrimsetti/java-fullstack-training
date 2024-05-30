da3953ce32d957fb1cb6ff05bd9a105f74172191
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 13:12:32 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/ConcateTwoStrings.java
@@ -0,0 +1,7 @@
+public class ConcateTwoStrings {
+    public static void main(String args[]){
+        String a = "Jyothika";
+        String b ="devi";
+        System.out.println(a + " " + b);
+    }
+}

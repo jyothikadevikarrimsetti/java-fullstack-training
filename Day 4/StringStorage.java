d620efcacf188f8dc804ed5d5629872ea9b5924a
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 16:37:05 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/StringStorage.java
@@ -0,0 +1,13 @@
+public class StringStorage {
+    public static void main(String args[]){
+        String a = "madam";
+        String b = "madam";
+        String c = new String("madam");
+        String d = new String("madam");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}

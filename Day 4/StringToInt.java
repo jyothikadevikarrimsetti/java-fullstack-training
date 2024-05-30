dadeca7ff6f40bd25c6405fc27bac4f22436697b
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 11:40:51 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/StringToInt.java
@@ -0,0 +1,9 @@
+public class StringToInt {
+    public static void main(String args[]){
+        String strNum = "1234";
+        int numInt = Integer.parseInt(strNum);
+        System.out.println("String: " + strNum);
+        System.out.println("Integer: " + numInt);
+
+    }
+}

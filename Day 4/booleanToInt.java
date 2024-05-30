0b5c41c93a496ed94df9dc3ed619c2f150c3afa3
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 11:08:22 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/booleanToInt.java
@@ -0,0 +1,7 @@
+public class booleanToInt {
+    public static void main(String args[]) {
+        boolean boolVal = true;
+        int intValue =boolVal ? 1 : 0;
+        System.out.println(boolVal);
+    }
+}
---
+++ src/ExplicitTypecasting.java
@@ -0,0 +1,10 @@
+public class ExplicitTypecasting {
+    public static void main(String args[]){
+        double i = 2832.2323;
+        short j =(short)i;
+        long k =(long)i;
+        System.out.println("before type casting"+i);
+        System.out.println("after type casting to short"+j);
+        System.out.println("after type casting to long"+k);
+    }
+}

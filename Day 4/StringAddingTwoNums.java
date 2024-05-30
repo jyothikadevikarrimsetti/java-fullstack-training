5f27540b8ac4aba6ade14f9fbfbc9002cdeacadc
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 13:28:57 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/StringAddingTwoNums.java
@@ -0,0 +1,8 @@
+public class StringAddingTwoNums {
+    public static void main(String args[]){
+        String a = "250";
+        String b = "750";
+        String c = a+b;
+        System.out.println(c);
+    }
+}

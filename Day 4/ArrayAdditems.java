d938898ded2b91ebd8244d8211fe0fc3311f462d
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-30 15:05:18 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/ArrayAdditems.java
@@ -0,0 +1,16 @@
+public class ArrayAdditems {
+    public static void main(String args[]) {
+        //int[] arr =new int[5];
+        //arr[0] =4;
+        //System.out.println(arr[2]);
+        import java.util.ArrayList;
+        ArrayList<String> person = new ArrayList<String>();
+        person.add("Jyo");
+        person.add("20");
+        person.add("B.Tech");
+        person.add("8'88");
+        person.add("2024");
+        System.out.println(person);
+    }
+}
+

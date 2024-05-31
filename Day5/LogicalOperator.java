0dfe2ca73eaad8e1d726ed40bb0d77d6797dc6d1
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-31 14:47:34 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day5/LogicalOperator.java
@@ -0,0 +1,12 @@
+public class LogicalOperator {
+    public static void main(String args[]){
+        int a = 200;
+        int b = 300;
+        if(a<20 || b<400) {
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}

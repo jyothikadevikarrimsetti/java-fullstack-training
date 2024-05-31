4d97874da24c997e069df359b7539de783734980
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-31 12:52:32 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day 4/ArithematicOperators.java
@@ -0,0 +1,17 @@
+public class ArithematicOperators {
+    public static void main(String args[]){
+        int a = 100;
+        int b = 10;
+        int sum = a+b;
+        int subtraction = a-b;
+        int multiplication =a*b;
+        int division = a/b;
+        int remainder = a%b;
+        System.out.println(sum);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(remainder);
+
+    }
+}

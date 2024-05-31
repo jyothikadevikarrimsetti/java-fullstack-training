e769b3cf07b6f02f64a204814fec5d87ed364c78
jyothikadevikarrimsetti <karrimsettijyothikadevi@gmail.com>
2024-05-31 14:41:59 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day5/ComparisionOperators.java
@@ -0,0 +1,12 @@
+public class ComparisionOperators {
+    public static void main(String[] args){
+        int a = 50;
+        int b = 20;
+        if(a>=b) {
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
